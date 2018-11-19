1fa91a63fed0a77ab55fca7aa094e0f183ba606c
Kresten <krestenma@gmail.com>
2018-11-19 17:47:30 +0100

    added mutations

--- server/src/schema.js
+++ server/src/schema.js
@@ -37,10 +37,29 @@ const typeDefs = gql`
     launches: [Launch]
   }
 
-  type Query {
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection {
+    cursor: String!
+    hasMore: Boolean!
     launches: [Launch]!
+  }
+
+  type Query {
+    launches(
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
     launch(id: ID!): Launch
-    # Queries for the current user
     me: User
   }
 
